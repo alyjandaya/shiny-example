b6ce7709fdc0e439c56dae38937482313fb3e3f0
alyjandaya <alyjandaya@gmail.com>
2016-07-26 10:21:41 -0500

    Update shiny-server.sh

--- shiny-server.sh
+++ shiny-server.sh
@@ -2,7 +2,7 @@
 
 # Make sure the directory for individual app logs exists
 sudo mkdir -p /var/log/shiny-server
-sudo chown -R shiny.shiny /var/log/shiny-server
+sudo chown shiny.shiny /var/log/shiny-server
 
-exec shiny-server >> /var/log/shiny-server.log 2>&1
+sudo exec shiny-server >> /var/log/shiny-server.log 2>&1
 
