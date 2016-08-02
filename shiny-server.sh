3d99a59a385b7d7ce5639d821df561cb324e55ce
alyjandaya <alyjandaya@gmail.com>
2016-08-02 12:56:05 -0500

    Update shiny-server.sh

--- shiny-server.sh
+++ shiny-server.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Make sure the directory for individual app logs exists
-mkdir -p Desktop/var/log/shiny-server
-chown shiny.shiny Desktop/var/log/shiny-server
+mkdir -p /var/log/shiny-server
+chown shiny.shiny /var/log/shiny-server
 
-exec shiny-server >> Desktop/var/log/shiny-server.log 2>&1
+exec shiny-server >> /var/log/shiny-server.log 2>&1
