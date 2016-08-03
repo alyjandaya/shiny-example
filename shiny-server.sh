cc4db3304c077357fb703c3759ecc6bfabbe08cc
alyjandaya <alyjandaya@gmail.com>
2016-08-03 09:15:28 -0500

    Update shiny-server.sh

--- shiny-server.sh
+++ shiny-server.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Make sure the directory for individual app logs exists
+# Make sure the directory for individual app logs exists,
 mkdir -p /var/log/shiny-server
 chown shiny.shiny /var/log/shiny-server
 
